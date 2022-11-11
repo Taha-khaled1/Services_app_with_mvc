ccb0e2b45fab94aad70d75b84acf33ba3f28a975
tahakhalid <tth31770@gmail.com>
2022-11-11 21:29:31 +0200

    v1

---
+++ lib/app/repositories/setting_repository.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+
+import '../models/address_model.dart';
+import '../models/setting_model.dart';
+import '../providers/laravel_provider.dart';
+
+class SettingRepository {
+  LaravelApiClient _laravelApiClient;
+
+  SettingRepository() {
+    this._laravelApiClient = Get.find<LaravelApiClient>();
+  }
+
+  Future<Setting> get() {
+    return _laravelApiClient.getSettings();
+  }
+
+  Future<List<dynamic>> getModules() {
+    return _laravelApiClient.getModules();
+  }
+
+  Future<Map<String, String>> getTranslations(String locale) {
+    return _laravelApiClient.getTranslations(locale);
+  }
+
+  Future<List<String>> getSupportedLocales() {
+    return _laravelApiClient.getSupportedLocales();
+  }
+
+  Future<List<Address>> getAddresses() {
+    return _laravelApiClient.getAddresses();
+  }
+}
